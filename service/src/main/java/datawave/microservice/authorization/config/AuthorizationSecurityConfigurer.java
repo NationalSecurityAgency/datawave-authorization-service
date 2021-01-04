29b85cc94c3e6dd61e90d858adb07b4a0c0ff542
Bill Oley <bill.oley@gmail.com>
2021-01-04 16:50:14 -0500

    Document differences in security between AuthorizationOperations and OAuthOperations along with the filter chain

--- service/src/main/java/datawave/microservice/authorization/config/AuthorizationSecurityConfigurer.java
+++ service/src/main/java/datawave/microservice/authorization/config/AuthorizationSecurityConfigurer.java
@@ -16,16 +16,40 @@ import org.springframework.security.web.authentication.preauth.PreAuthenticatedA
 import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
 
 /**
- * Configures security for the spring boot application. This configuration ensures that only allowed callers can talk to us, and the caller can supply either a
- * signed JSON Web Token containing the authorization credentials or can authenticate by using the X-ProxiedEntitiesChain and X-ProxiedIssuersChain headers that
- * are supplied by trusted callers.
  * <p>
- * Note that for authentication purposes, it is assumed that another service is authenticating on behalf of a user, and therefore <em>only</em> the
- * X-ProxiedEntitiesChain/X-ProxiedIssuersChain values are used to calculate the authenticated principal and the incoming certificate is only used to determine
- * if the caller is trusted. This behavior can be change if {@link DatawaveSecurityProperties#isProxiedEntitiesRequired()} is set to false. In that case, if the
- * caller supplies no X-ProxiedEntitiesChain/X-ProxiedIssuersChain header, the incoming certificate subject and issuer are copied into the
- * X-ProxiedEntitiesChain/X-ProxiedIssuersChain header so that the caller is proxying for itself. This is a convenience used for testing when one would like a
- * user to be able to directly access the authorization service (e.g., from a web browser).
+ * Configures security for the authorization microservice.
+ * <p>
+ * <u>AuthorizationOperations</u>:
+ * <p>
+ * Only allowed callers can call these endpoints. A caller can supply a signed JSON Web Token (JWT) containing authorization credentials or can authenticate by
+ * using a client certificate (https) or via X-SSL-clientcert-subject/X-SSL-clientcert-issuer http headers (http) to establish that they are a trusted caller.
+ * <p>
+ * Callers using a client certificate or trusted headers usually supply X-ProxiedEntitiesChain/X-ProxiedIssuersChain http headers because it is assumed that
+ * another service is authenticating on behalf of a user, and therefore <em>only</em> the X-ProxiedEntitiesChain/X-ProxiedIssuersChain values are used to
+ * calculate the authenticated principal and the caller's information is only used to determine if the caller is trusted. This behavior can be changed if
+ * {@link DatawaveSecurityProperties#isProxiedEntitiesRequired()} is set to false. In that case, if the caller supplies no
+ * X-ProxiedEntitiesChain/X-ProxiedIssuersChain header, then the caller's information is copied into the X-ProxiedEntitiesChain/X-ProxiedIssuersChain header so
+ * that the caller is proxying for itself. This is a convenience used for testing when one would like a user to be able to directly access
+ * AuthorizationOperations (e.g., from a web browser).
+ * <p>
+ * <u>OAuthOperations</u>:
+ * <p>
+ * Security is maintained through a list of registered clients (client_id, client_secret), redirects, and time limitations which allow any user to call
+ * oauth/authorize as part of the OAuth2 code flow when using a registered client service. This flow made it necessary to bypass the allowedCallers check that
+ * is used for the AuthorizationOperations.
+ * <p>
+ * <u>Security Filter Chain</u>:
+ * <p>
+ * AuthorizationAllowedCallersFilter (extends AllowedCallersFilter) is executed before the spring X509AuthenticationFilter and will check for allowedCallers if
+ * there is a client certificate. The allowedCallers check is bypassed for paths with an oauth prefix.
+ * <p>
+ * JWTAuthenticationFilter checks if there is a JWT Bearer token in the Authorization http header. If a valid JWT is found, then that identity is used.
+ * <p>
+ * AuthorizationProxiedEntityX509Filter (extends ProxiedEntityX509Filter) checks if authorization has already happened (JWT). If not, then the configured
+ * UserDetailsService is used. For the authorization microservice, this is the ProxiedEntityUserDetailsService. AuthorizationProxiedEntityX509Filter overrides
+ * getPreAuthenticatedPrincipal() to call super.getPreAuthenticatedPrincipal() for a ProxiedEntityPreauthPrincipal and then if non-null creates a subclass
+ * AuthorizationProxiedEntityPreauthPrincipal which contains the HttpServletRequest. This allows ProxiedEntityUserDetailsService to call
+ * AuthorizationAllowedCallersFilter.enforceAllowedCallersForRequest() to determine if allowedCallers should enforced for that request.
  */
 @Order(SecurityProperties.BASIC_AUTH_ORDER - 3)
 @Configuration
